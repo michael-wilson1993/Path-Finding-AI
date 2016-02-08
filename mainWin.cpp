799910be20cb33a9f720d5eafc9aee3f519c5c59
michael-wilson1993 <wilsonm_1993@hotmail.com>
2016-02-07 16:32:33 -0800

    testing atm

--- mainWin.cpp
+++ mainWin.cpp
@@ -1,29 +1,29 @@
 #include<QtGui>
-
 #include "mainWin.h"
 #include "canvas.h"
-
+// main qt window
 mainWin::mainWin()
 {
 
    char c = 'd';
    canvas = new Canvas(this);
-   next = new QPushButton("next step");
-   r = new QPushButton("close");
+   next = new QPushButton("Go");
+   close = new QPushButton("close");
+   pause = new QPushButton("pause");
+   reset = new QPushButton("reset");
 
    connect(next, SIGNAL(clicked()), this, SLOT(updateSearch()));
-   connect(r, SIGNAL(clicked()), this, SLOT(close()));
+   connect(close, SIGNAL(clicked()), this, SLOT(close()));
 
    QGridLayout *layout = new QGridLayout;
    layout -> addWidget(canvas,0,0,4,4);
    layout -> addWidget(next,5,1,1,2);
-   layout -> addWidget(r,5,3,1,1);
+   layout -> addWidget(close,5,3,1,1);
 
    setLayout(layout);
 
    timer = new QTimer(this);
    connect(timer, SIGNAL(timeout()), this, SLOT(updateSearch()));
-   //timer->start(1000);
 }
 
 void mainWin::updateSearch()
